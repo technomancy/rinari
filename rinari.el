d010c174e03a3ed5e98cd2d92a73b9aaa4de3037
Eric Schulte <schulte.eric@gmail.com>
2008-07-22 19:16:12 -0700

    added execution of rails scripts rinari-script

--- rinari.el
+++ rinari.el
@@ -159,8 +159,7 @@ argument to edit command line options."
     (save-excursion (pop-to-buffer "*ruby*")
       (set (make-local-variable 'inferior-ruby-first-prompt-pattern) "^>> ")
       (set (make-local-variable 'inferior-ruby-prompt-pattern) "^>> ")
-      (rinari-launch)
-      (rename-buffer "*console*"))))
+      (rinari-launch))))
 
 (defun rinari-sql ()
   "Browse the application's database.  Looks up login information
