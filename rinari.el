763ea013cb8dfd2a664187d2c1dea718a17e4f80
Eric Schulte <schulte.eric@gmail.com>
2008-09-08 16:50:20 -0700

    attempting windows support for movement functions

--- rinari.el
+++ rinari.el
@@ -311,118 +311,236 @@ renders and redirects to find the final controller or view."
 
 (setf
  rinari-jump-schema
- '((model
-    "m"
-    (("app/controllers/\\1_controller.rb#\\2"  . "app/models/\\1.rb#\\2")
-     ("app/views/\\1/.*"                       . "app/models/\\1.rb")
-     ("app/helpers/\\1_helper.rb"              . "app/models/\\1.rb")
-     ("db/migrate/.*create_\\1.rb"             . "app/models/\\1.rb")
-     ("test/functional/\\1_controller_test.rb" . "app/models/\\1.rb")
-     ("test/unit/\\1_test.rb#test_\\2"         . "app/models/\\1.rb#\\2")
-     ("test/unit/\\1_test.rb"                  . "app/models/\\1.rb")
-     ("test/fixtures/\\1.yml"                  . "app/models/\\1.rb")
-     (t                                        . "app/models/"))
-    (lambda (path)
-      (rinari-generate "model"
-		       (and (string-match ".*/\\(.+?\\)\.rb" path)
-			    (match-string 1 path)))))
-   (controller
-    "c"
-    (("app/models/\\1.rb"                      . "app/controllers/\\1_controller.rb")
-     ("app/views/\\1/\\2\\..*"                 . "app/controllers/\\1_controller.rb#\\2")
-     ("app/helpers/\\1_helper.rb"              . "app/controllers/\\1_controller.rb")
-     ("db/migrate/.*create_\\1.rb"             . "app/controllers/\\1_controller.rb")
-     ("test/functional/\\1_test.rb#test_\\2"   . "app/controllers/\\1.rb#\\2")
-     ("test/functional/\\1_test.rb"            . "app/controllers/\\1.rb")
-     ("test/unit/\\1_test.rb#test_\\2"         . "app/controllers/\\1_controller.rb#\\2")
-     ("test/unit/\\1_test.rb"                  . "app/controllers/\\1_controller.rb")
-     ("test/fixtures/\\1.yml"                  . "app/controllers/\\1_controller.rb")
-     (t                                        . "app/controllers/"))
-    (lambda (path)
-      (rinari-generate "controller"
-		       (and (string-match ".*/\\(.+?\\)_controller\.rb" path)
-			    (match-string 1 path)))))
-   (view
-    "v"
-    (("app/models/\\1.rb"                      . "app/views/\\1/.*")
-     ((lambda () ;; find the controller/view
-	(let* ((raw-file (and (buffer-file-name)
-			      (file-name-nondirectory (buffer-file-name))))
-	       (file (and raw-file
-			  (string-match "^\\(.*\\)_controller.rb" raw-file)
-			  (match-string 1 raw-file))) ;; controller
-	       (raw-method (ruby-add-log-current-method))
-	       (method (and file raw-method ;; action
-			    (string-match "#\\(.*\\)" raw-method)
-			    (match-string 1 raw-method))))
-	  (if (and file method) (rinari-follow-controller-and-action file method))))
-                                               . "app/views/\\1/\\2\\..*")
-     ("app/helpers/\\1_helper.rb"              . "app/views/\\1/.*")
-     ("db/migrate/.*create_\\1.rb"             . "app/views/\\1/.*")
-     ("test/functional/\\1_controller_test.rb" . "app/views/\\1/.*")
-     ("test/unit/\\1_test.rb#test_\\2"         . "app/views/\\1/_?\\2.*")
-     ("test/fixtures/\\1.yml"                  . "app/views/\\1/.*")
-     (t                                        . "app/views/.*"))
-    nil)
-   (test
-    "t"
-    (("app/models/\\1.rb#\\2"                  . "test/unit/\\1_test.rb#test_\\2")
-     ("app/controllers/\\1.rb#\\2"             . "test/functional/\\1_test.rb#test_\\2")
-     ("app/views/\\1/_?\\2\\..*"               . "test/functional/\\1_controller_test.rb#test_\\2")
-     ("app/helpers/\\1_helper.rb"              . "test/functional/\\1_controller_test.rb")
-     ("db/migrate/.*create_\\1.rb"             . "test/unit/\\1_test.rb")
-     ("test/functional/\\1_controller_test.rb" . "test/unit/\\1_test.rb")
-     ("test/unit/\\1_test.rb"                  . "test/functional/\\1_controller_test.rb")
-     (t                                        . "test/.*"))
-    nil)
-   (fixture
-    "x"
-    (("app/models/\\1.rb"                      . "test/fixtures/\\1.yml")
-     ("app/controllers/\\1_controller.rb"      . "test/fixtures/\\1.yml")
-     ("app/views/\\1/.*"                       . "test/fixtures/\\1.yml")
-     ("app/helpers/\\1_helper.rb"              . "test/fixtures/\\1.yml")
-     ("db/migrate/.*create_\\1.rb"             . "test/fixtures/\\1.yml")
-     ("test/functional/\\1_controller_test.rb" . "test/fixtures/\\1.yml")
-     ("test/unit/\\1_test.rb"                  . "test/fixtures/\\1.yml")
-     (t                                        . "test/fixtures/"))
-    nil)
-   (helper
-    "h"
-    (("app/models/\\1.rb"                      . "app/helpers/\\1_helper.rb")
-     ("app/controllers/\\1_controller.rb"      . "app/helpers/\\1_helper.rb")
-     ("app/views/\\1/.*"                       . "app/helpers/\\1_helper.rb")
-     ("app/helpers/\\1_helper.rb"              . "app/helpers/\\1_helper.rb")
-     ("db/migrate/.*create_\\1.rb"             . "app/helpers/\\1_helper.rb")
-     ("test/functional/\\1_controller_test.rb" . "app/helpers/\\1_helper.rb")
-     ("test/unit/\\1_test.rb#test_\\2"         . "app/helpers/\\1_helper.rb#\\2")
-     ("test/unit/\\1_test.rb"                  . "app/helpers/\\1_helper.rb")
-     (t                                        . "app/helpers/"))
-    nil)
-   (migration
-    "i"
-    (("app/controllers/\\1_controller.rb"      . "db/migrate/.*create_\\1.rb")
-     ("app/views/\\1/.*"                       . "db/migrate/.*create_\\1.rb")
-     ("app/helpers/\\1_helper.rb"              . "db/migrate/.*create_\\1.rb")
-     ("app/models/\\1.rb"                      . "db/migrate/.*create_\\1.rb")
-     ("test/functional/\\1_controller_test.rb" . "db/migrate/.*create_\\1.rb")
-     ("test/unit/\\1_test.rb#test_\\2"         . "db/migrate/.*create_\\1.rb#\\2")
-     ("test/unit/\\1_test.rb"                  . "db/migrate/.*create_\\1.rb")
-     (t                                        . "db/migrate/"))
-    (lambda (path)
-      (rinari-generate "migration"
-		       (and (string-match ".*create_\\(.+?\\)\.rb" path)
-			    (match-string 1 path)))))
-   (environment "e" ((t . "config/environments/")) nil)
-   (configuration "n" ((t . "config/")) nil)
-   (script "s" ((t . "script/")) nil)
-   (lib "l" ((t . "lib/")) nil)
-   (log "o" ((t . "log/")) nil)
-   (worker "w" ((t . "lib/workers/")) nil)
-   (public "p" ((t . "public/")) nil)
-   (stylesheet "y" ((t . "public/stylesheets/.*")) nil)
-   (javascript "j" ((t . "public/javascripts/.*")) nil)
-   (plugin "l" ((t . "vendor/plugins/")) nil)
-   (file-in-project "f" ((t . ".*")) nil)))
+ (if (or (equal system-type 'ms-dos)
+	 (equal system-type 'windows-nt))
+     ;; movement schema for window$ systems
+     '((model
+	"m"
+	(("app\\controllers\\\\1_controller.rb#\\2"  . "app\\models\\\\1.rb#\\2")
+	 ("app\\views\\\\1\\.*"                       . "app\\models\\\\1.rb")
+	 ("app\\helpers\\\\1_helper.rb"              . "app\\models\\\\1.rb")
+	 ("db\\migrate\\.*create_\\1.rb"             . "app\\models\\\\1.rb")
+	 ("test\\functional\\\\1_controller_test.rb" . "app\\models\\\\1.rb")
+	 ("test\\unit\\\\1_test.rb#test_\\2"         . "app\\models\\\\1.rb#\\2")
+	 ("test\\unit\\\\1_test.rb"                  . "app\\models\\\\1.rb")
+	 ("test\\fixtures\\\\1.yml"                  . "app\\models\\\\1.rb")
+	 (t                                        . "app\\models\\"))
+	(lambda (path)
+	  (rinari-generate "model"
+			   (and (string-match ".*\\\\(.+?\\)\.rb" path)
+				(match-string 1 path)))))
+       (controller
+	"c"
+	(("app\\models\\\\1.rb"                      . "app\\controllers\\\\1_controller.rb")
+	 ("app\\views\\\\1\\\\2\\..*"                 . "app\\controllers\\\\1_controller.rb#\\2")
+	 ("app\\helpers\\\\1_helper.rb"              . "app\\controllers\\\\1_controller.rb")
+	 ("db\\migrate\\.*create_\\1.rb"             . "app\\controllers\\\\1_controller.rb")
+	 ("test\\functional\\\\1_test.rb#test_\\2"   . "app\\controllers\\\\1.rb#\\2")
+	 ("test\\functional\\\\1_test.rb"            . "app\\controllers\\\\1.rb")
+	 ("test\\unit\\\\1_test.rb#test_\\2"         . "app\\controllers\\\\1_controller.rb#\\2")
+	 ("test\\unit\\\\1_test.rb"                  . "app\\controllers\\\\1_controller.rb")
+	 ("test\\fixtures\\\\1.yml"                  . "app\\controllers\\\\1_controller.rb")
+	 (t                                        . "app\\controllers\\"))
+	(lambda (path)
+	  (rinari-generate "controller"
+			   (and (string-match ".*\\\\(.+?\\)_controller\.rb" path)
+				(match-string 1 path)))))
+       (view
+	"v"
+	(("app\\models\\\\1.rb"                      . "app\\views\\\\1\\.*")
+	 ((lambda () ;; find the controller\\view
+	    (let* ((raw-file (and (buffer-file-name)
+				  (file-name-nondirectory (buffer-file-name))))
+		   (file (and raw-file
+			      (string-match "^\\(.*\\)_controller.rb" raw-file)
+			      (match-string 1 raw-file))) ;; controller
+		   (raw-method (ruby-add-log-current-method))
+		   (method (and file raw-method ;; action
+				(string-match "#\\(.*\\)" raw-method)
+				(match-string 1 raw-method))))
+	      (if (and file method) (rinari-follow-controller-and-action file method))))
+	  . "app\\views\\\\1\\\\2\\..*")
+	 ("app\\helpers\\\\1_helper.rb"              . "app\\views\\\\1\\.*")
+	 ("db\\migrate\\.*create_\\1.rb"             . "app\\views\\\\1\\.*")
+	 ("test\\functional\\\\1_controller_test.rb" . "app\\views\\\\1\\.*")
+	 ("test\\unit\\\\1_test.rb#test_\\2"         . "app\\views\\\\1\\_?\\2.*")
+	 ("test\\fixtures\\\\1.yml"                  . "app\\views\\\\1\\.*")
+	 (t                                        . "app\\views\\.*"))
+	nil)
+       (test
+	"t"
+	(("app\\models\\\\1.rb#\\2"                  . "test\\unit\\\\1_test.rb#test_\\2")
+	 ("app\\controllers\\\\1.rb#\\2"             . "test\\functional\\\\1_test.rb#test_\\2")
+	 ("app\\views\\\\1\\_?\\2\\..*"               . "test\\functional\\\\1_controller_test.rb#test_\\2")
+	 ("app\\helpers\\\\1_helper.rb"              . "test\\functional\\\\1_controller_test.rb")
+	 ("db\\migrate\\.*create_\\1.rb"             . "test\\unit\\\\1_test.rb")
+	 ("test\\functional\\\\1_controller_test.rb" . "test\\unit\\\\1_test.rb")
+	 ("test\\unit\\\\1_test.rb"                  . "test\\functional\\\\1_controller_test.rb")
+	 (t                                        . "test\\.*"))
+	nil)
+       (fixture
+	"x"
+	(("app\\models\\\\1.rb"                      . "test\\fixtures\\\\1.yml")
+	 ("app\\controllers\\\\1_controller.rb"      . "test\\fixtures\\\\1.yml")
+	 ("app\\views\\\\1\\.*"                       . "test\\fixtures\\\\1.yml")
+	 ("app\\helpers\\\\1_helper.rb"              . "test\\fixtures\\\\1.yml")
+	 ("db\\migrate\\.*create_\\1.rb"             . "test\\fixtures\\\\1.yml")
+	 ("test\\functional\\\\1_controller_test.rb" . "test\\fixtures\\\\1.yml")
+	 ("test\\unit\\\\1_test.rb"                  . "test\\fixtures\\\\1.yml")
+	 (t                                        . "test\\fixtures\\"))
+	nil)
+       (helper
+	"h"
+	(("app\\models\\\\1.rb"                      . "app\\helpers\\\\1_helper.rb")
+	 ("app\\controllers\\\\1_controller.rb"      . "app\\helpers\\\\1_helper.rb")
+	 ("app\\views\\\\1\\.*"                       . "app\\helpers\\\\1_helper.rb")
+	 ("app\\helpers\\\\1_helper.rb"              . "app\\helpers\\\\1_helper.rb")
+	 ("db\\migrate\\.*create_\\1.rb"             . "app\\helpers\\\\1_helper.rb")
+	 ("test\\functional\\\\1_controller_test.rb" . "app\\helpers\\\\1_helper.rb")
+	 ("test\\unit\\\\1_test.rb#test_\\2"         . "app\\helpers\\\\1_helper.rb#\\2")
+	 ("test\\unit\\\\1_test.rb"                  . "app\\helpers\\\\1_helper.rb")
+	 (t                                        . "app\\helpers\\"))
+	nil)
+       (migration
+	"i"
+	(("app\\controllers\\\\1_controller.rb"      . "db\\migrate\\.*create_\\1.rb")
+	 ("app\\views\\\\1\\.*"                       . "db\\migrate\\.*create_\\1.rb")
+	 ("app\\helpers\\\\1_helper.rb"              . "db\\migrate\\.*create_\\1.rb")
+	 ("app\\models\\\\1.rb"                      . "db\\migrate\\.*create_\\1.rb")
+	 ("test\\functional\\\\1_controller_test.rb" . "db\\migrate\\.*create_\\1.rb")
+	 ("test\\unit\\\\1_test.rb#test_\\2"         . "db\\migrate\\.*create_\\1.rb#\\2")
+	 ("test\\unit\\\\1_test.rb"                  . "db\\migrate\\.*create_\\1.rb")
+	 (t                                        . "db\\migrate\\"))
+	(lambda (path)
+	  (rinari-generate "migration"
+			   (and (string-match ".*create_\\(.+?\\)\.rb" path)
+				(match-string 1 path)))))
+       (environment "e" ((t . "config\\environments\\")) nil)
+       (configuration "n" ((t . "config\\")) nil)
+       (script "s" ((t . "script\\")) nil)
+       (lib "l" ((t . "lib\\")) nil)
+       (log "o" ((t . "log\\")) nil)
+       (worker "w" ((t . "lib\\workers\\")) nil)
+       (public "p" ((t . "public\\")) nil)
+       (stylesheet "y" ((t . "public\\stylesheets\\.*")) nil)
+       (javascript "j" ((t . "public\\javascripts\\.*")) nil)
+       (plugin "l" ((t . "vendor\\plugins\\")) nil)
+       (file-in-project "f" ((t . ".*")) nil))
+
+   ;; movement schema for posix systems
+   '((model
+      "m"
+      (("app/controllers/\\1_controller.rb#\\2"  . "app/models/\\1.rb#\\2")
+       ("app/views/\\1/.*"                       . "app/models/\\1.rb")
+       ("app/helpers/\\1_helper.rb"              . "app/models/\\1.rb")
+       ("db/migrate/.*create_\\1.rb"             . "app/models/\\1.rb")
+       ("test/functional/\\1_controller_test.rb" . "app/models/\\1.rb")
+       ("test/unit/\\1_test.rb#test_\\2"         . "app/models/\\1.rb#\\2")
+       ("test/unit/\\1_test.rb"                  . "app/models/\\1.rb")
+       ("test/fixtures/\\1.yml"                  . "app/models/\\1.rb")
+       (t                                        . "app/models/"))
+      (lambda (path)
+	(rinari-generate "model"
+			 (and (string-match ".*/\\(.+?\\)\.rb" path)
+			      (match-string 1 path)))))
+     (controller
+      "c"
+      (("app/models/\\1.rb"                      . "app/controllers/\\1_controller.rb")
+       ("app/views/\\1/\\2\\..*"                 . "app/controllers/\\1_controller.rb#\\2")
+       ("app/helpers/\\1_helper.rb"              . "app/controllers/\\1_controller.rb")
+       ("db/migrate/.*create_\\1.rb"             . "app/controllers/\\1_controller.rb")
+       ("test/functional/\\1_test.rb#test_\\2"   . "app/controllers/\\1.rb#\\2")
+       ("test/functional/\\1_test.rb"            . "app/controllers/\\1.rb")
+       ("test/unit/\\1_test.rb#test_\\2"         . "app/controllers/\\1_controller.rb#\\2")
+       ("test/unit/\\1_test.rb"                  . "app/controllers/\\1_controller.rb")
+       ("test/fixtures/\\1.yml"                  . "app/controllers/\\1_controller.rb")
+       (t                                        . "app/controllers/"))
+      (lambda (path)
+	(rinari-generate "controller"
+			 (and (string-match ".*/\\(.+?\\)_controller\.rb" path)
+			      (match-string 1 path)))))
+     (view
+      "v"
+      (("app/models/\\1.rb"                      . "app/views/\\1/.*")
+       ((lambda () ;; find the controller/view
+	  (let* ((raw-file (and (buffer-file-name)
+				(file-name-nondirectory (buffer-file-name))))
+		 (file (and raw-file
+			    (string-match "^\\(.*\\)_controller.rb" raw-file)
+			    (match-string 1 raw-file))) ;; controller
+		 (raw-method (ruby-add-log-current-method))
+		 (method (and file raw-method ;; action
+			      (string-match "#\\(.*\\)" raw-method)
+			      (match-string 1 raw-method))))
+	    (if (and file method) (rinari-follow-controller-and-action file method))))
+	. "app/views/\\1/\\2\\..*")
+       ("app/helpers/\\1_helper.rb"              . "app/views/\\1/.*")
+       ("db/migrate/.*create_\\1.rb"             . "app/views/\\1/.*")
+       ("test/functional/\\1_controller_test.rb" . "app/views/\\1/.*")
+       ("test/unit/\\1_test.rb#test_\\2"         . "app/views/\\1/_?\\2.*")
+       ("test/fixtures/\\1.yml"                  . "app/views/\\1/.*")
+       (t                                        . "app/views/.*"))
+      nil)
+     (test
+      "t"
+      (("app/models/\\1.rb#\\2"                  . "test/unit/\\1_test.rb#test_\\2")
+       ("app/controllers/\\1.rb#\\2"             . "test/functional/\\1_test.rb#test_\\2")
+       ("app/views/\\1/_?\\2\\..*"               . "test/functional/\\1_controller_test.rb#test_\\2")
+       ("app/helpers/\\1_helper.rb"              . "test/functional/\\1_controller_test.rb")
+       ("db/migrate/.*create_\\1.rb"             . "test/unit/\\1_test.rb")
+       ("test/functional/\\1_controller_test.rb" . "test/unit/\\1_test.rb")
+       ("test/unit/\\1_test.rb"                  . "test/functional/\\1_controller_test.rb")
+       (t                                        . "test/.*"))
+      nil)
+     (fixture
+      "x"
+      (("app/models/\\1.rb"                      . "test/fixtures/\\1.yml")
+       ("app/controllers/\\1_controller.rb"      . "test/fixtures/\\1.yml")
+       ("app/views/\\1/.*"                       . "test/fixtures/\\1.yml")
+       ("app/helpers/\\1_helper.rb"              . "test/fixtures/\\1.yml")
+       ("db/migrate/.*create_\\1.rb"             . "test/fixtures/\\1.yml")
+       ("test/functional/\\1_controller_test.rb" . "test/fixtures/\\1.yml")
+       ("test/unit/\\1_test.rb"                  . "test/fixtures/\\1.yml")
+       (t                                        . "test/fixtures/"))
+      nil)
+     (helper
+      "h"
+      (("app/models/\\1.rb"                      . "app/helpers/\\1_helper.rb")
+       ("app/controllers/\\1_controller.rb"      . "app/helpers/\\1_helper.rb")
+       ("app/views/\\1/.*"                       . "app/helpers/\\1_helper.rb")
+       ("app/helpers/\\1_helper.rb"              . "app/helpers/\\1_helper.rb")
+       ("db/migrate/.*create_\\1.rb"             . "app/helpers/\\1_helper.rb")
+       ("test/functional/\\1_controller_test.rb" . "app/helpers/\\1_helper.rb")
+       ("test/unit/\\1_test.rb#test_\\2"         . "app/helpers/\\1_helper.rb#\\2")
+       ("test/unit/\\1_test.rb"                  . "app/helpers/\\1_helper.rb")
+       (t                                        . "app/helpers/"))
+      nil)
+     (migration
+      "i"
+      (("app/controllers/\\1_controller.rb"      . "db/migrate/.*create_\\1.rb")
+       ("app/views/\\1/.*"                       . "db/migrate/.*create_\\1.rb")
+       ("app/helpers/\\1_helper.rb"              . "db/migrate/.*create_\\1.rb")
+       ("app/models/\\1.rb"                      . "db/migrate/.*create_\\1.rb")
+       ("test/functional/\\1_controller_test.rb" . "db/migrate/.*create_\\1.rb")
+       ("test/unit/\\1_test.rb#test_\\2"         . "db/migrate/.*create_\\1.rb#\\2")
+       ("test/unit/\\1_test.rb"                  . "db/migrate/.*create_\\1.rb")
+       (t                                        . "db/migrate/"))
+      (lambda (path)
+	(rinari-generate "migration"
+			 (and (string-match ".*create_\\(.+?\\)\.rb" path)
+			      (match-string 1 path)))))
+     (environment "e" ((t . "config/environments/")) nil)
+     (configuration "n" ((t . "config/")) nil)
+     (script "s" ((t . "script/")) nil)
+     (lib "l" ((t . "lib/")) nil)
+     (log "o" ((t . "log/")) nil)
+     (worker "w" ((t . "lib/workers/")) nil)
+     (public "p" ((t . "public/")) nil)
+     (stylesheet "y" ((t . "public/stylesheets/.*")) nil)
+     (javascript "j" ((t . "public/javascripts/.*")) nil)
+     (plugin "l" ((t . "vendor/plugins/")) nil)
+     (file-in-project "f" ((t . ".*")) nil))
+   ))
 
 (mapcar
  (lambda (type)
