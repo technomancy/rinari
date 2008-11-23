2d0eaa470ed481fc662ec9b1858bc645403b141f
Eric Schulte <schulte.eric@gmail.com>
2008-11-22 16:14:39 -0800

    replacing run-ruby in inf-ruby.el

--- util/inf-ruby.el
+++ util/inf-ruby.el
@@ -191,8 +191,25 @@ run)."
     (setq inf-ruby-buffer buffer-name)
     (pop-to-buffer buffer-name)))
 
-(defalias 'run-ruby 'inf-ruby
-  "An alias for backwards-compatibility.")
+(defun run-ruby (cmd)
+  "Run an inferior Ruby process, input and output via buffer *ruby*.
+If there is a process already running in `*ruby*', switch to that buffer.
+With argument, allows you to edit the command line (default is value
+of `ruby-program-name').  Runs the hooks `inferior-ruby-mode-hook'
+\(after the `comint-mode-hook' is run).
+\(Type \\[describe-mode] in the process buffer for a list of commands.)"
+
+  (interactive (list (if current-prefix-arg
+			 (read-string "Run Ruby: " ruby-program-name)
+			 ruby-program-name)))
+  (if (not (comint-check-proc "*ruby*"))
+      (let ((cmdlist (split-string cmd)))
+	(set-buffer (apply 'make-comint "ruby" (car cmdlist)
+			   nil (cdr cmdlist)))
+	(inf-ruby-mode)))
+  (setq ruby-program-name cmd)
+  (setq ruby-buffer "*ruby*")
+  (pop-to-buffer "*ruby*"))
 
 (defun inf-ruby-proc ()
   "Returns the current IRB process. See variable inf-ruby-buffer."
